156d2641c659bacecd796ba6566030b106e7ee55
Patryk <61631089+patrykf03@users.noreply.github.com>
2022-10-02 20:55:30 +0000

    update notion ver

--- notion-repackaged.sh
+++ notion-repackaged.sh
@@ -4,13 +4,13 @@
 #
 
 # Version of the original Notion App installer to repackage
-export NOTION_VERSION=2.0.18
+export NOTION_VERSION=2.0.32
 
 # Revision of the current version
 export NOTION_REPACKAGED_REVISION=1
 
 # The md5sum hash of the downloaded .exe for the installer
-export NOTION_DOWNLOAD_HASH=31bb8b9d547cb5385b9f04017214a206
+export NOTION_DOWNLOAD_HASH=5ac71d76f6b08ca8d86992d3bbf7cc0c
 
 # The commit of notion-enhancer/desktop to target
 export NOTION_ENHANCER_DESKTOP_COMMIT=832db26d2e6a247d9e76847b2de50435fa7c40d2
